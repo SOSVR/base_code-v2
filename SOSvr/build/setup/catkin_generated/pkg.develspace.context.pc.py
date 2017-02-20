92cfe91820c6730442a8a6192aef9f5c41222e42
Admin <hoseingohari76@gmail.com>
2017-02-20 16:51:43 +0330

    added SOSvr package

---
+++ SOSvr/build/setup/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "nav_msgs;roscpp;rospy;tf;sensor_msgs;cv_bridge;std_msgs;image_transport".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "setup"
+PROJECT_SPACE_DIR = "/home/user/workspace/sos/SOSvr/devel"
+PROJECT_VERSION = "0.0.0"
