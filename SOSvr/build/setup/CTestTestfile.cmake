3a2da45810d60852af4dad802e5486d6e5193d6f
taherahmadi <14taher@gmail.com>
2017-03-04 20:32:54 +0330

    ---shera deleted

--- SOSvr/build/setup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/workspace/sos/SOSvr/src/setup
-# Build directory: /home/user/workspace/sos/SOSvr/build/setup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
