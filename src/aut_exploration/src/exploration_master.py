6d9602b1ffe1ff62dac4c8b7f39f4a8ada5a7c3c
Hossein76 <hoseingohari76@gmail.com>
2017-04-25 14:57:18 +0430

    exploration server under development

--- src/aut_exploration/src/exploration_master.py
+++ src/aut_exploration/src/exploration_master.py
@@ -11,20 +11,30 @@ from nav_msgs.msg import *;
 from std_msgs.msg import Header;
 from tf import TransformListener;
 #from rail_object_detector.msg import *;
+merged_map=None;
+
+def setMap(rawMap):
+    global merged_map;
+    merged_map=rawMap;
+
+
+def start_listening():
+    print ("Reading the merged map data for exploration server");
+    rospy.Subscriber("/map", OccupancyGrid, setMap);
+
 
 def find_far_goal1(req):
-    pass;
+    return FarGoalResponse();
 
 def start_server():
     rospy.init_node("exploration server");
+    start_listening();
     rospy.loginfo("starting the exploration server");
     s = rospy.Service("/sos1/request_fargoal", FarGoal, find_far_goal1);
     s = rospy.Service("/sos2/request_fargoal", FarGoal, find_far_goal2);
     s = rospy.Service("/sos3/request_fargoal", FarGoal, find_far_goal3);
     s = rospy.Service("/sos4/request_fargoal", FarGoal, find_far_goal4);
     rospy.spin();
-
-
 
 
 if __name__ == '__main__':
